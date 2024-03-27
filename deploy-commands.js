0f57f632fcb6d8f6e183fabb08365d5e0765c413
Karl Løvendahl <kalle44@live.dk>
2024-03-27 20:22:32 +0100

    added deploy-commands and events folder now fully working! remeber to execute deploy-commands.js to deploy newly created commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,19 +1,20 @@
-require('dotenv').config();
+const { REST, Routes } = require('discord.js');
 
+require('dotenv').config();
+const token = process.env.TOKEN;
 const clientId = process.env.clientId;
 const guildId = process.env.guildId;
-const token = process.env.token;
-const { REST, Routes } = require('discord.js');
+
 const fs = require('node:fs');
 const path = require('node:path');
 
 const commands = [];
-// Grab all the command folders from the commands directory you created earlier
+// Grab all the command folders from the commands dir 
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-  // Grab all the command files from the commands directory you created earlier
+  // Grab all the command files from the commands dir 
   const commandsPath = path.join(foldersPath, folder);
   const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
   // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
@@ -28,23 +29,22 @@ for (const folder of commandFolders) {
   }
 }
 
-// Construct and prepare an instance of the REST module
+// COnstruct and prepare an instance of the REST module
 const rest = new REST().setToken(token);
 
-// and deploy your commands!
+// and deploy commands
 (async () => {
   try {
-    console.log(`Started refreshing ${commands.length} application (/) commands.`);
+    console.log(`Started refreshing ${commands.length} application (/) commands.`)
 
-    // The put method is used to fully refresh all commands in the guild with the current set
+    // The put method is used to fully refresh all commands in the guild with the current set 
     const data = await rest.put(
       Routes.applicationGuildCommands(clientId, guildId),
       { body: commands },
     );
 
-    console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    console.log(`Successfully reloaded ${data.length} appliaction (/) commands.`);
   } catch (error) {
-    // And of course, make sure you catch and log any errors!
     console.error(error);
   }
 })();
--- index.js
+++ index.js
@@ -1,33 +1,58 @@
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+// File system module - Node's native - used to read commands dir
+const fs = require('node:fs');
+// Path utility to construct paths to access files and dirs
+const path = require('node:path');
 require('dotenv').config();
-const prefix = "!";
+
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+
+
+// Collection class extends JavaScripts native Map class, and has more extensive and useful functionality.
+// Used to store and effeciently retrieve commands for execution
 
 // Create a new client instance
 const token = process.env.TOKEN;
-const client = new Client({ intents: [
-  GatewayIntentBits.Guilds,
-  GatewayIntentBits.GuildMessages,
-  GatewayIntentBits.MessageContent, 
-] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    // GatewayIntentBits.GuildMessages,
+    // GatewayIntentBits.MessageContent,
+  ]
+});
+client.commands = new Collection();
 
-client.on('messageCreate', message => {
-  console.log(message.content);
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+// Importing commands from commands folder using fs and path
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
-  const args = message.content.slice(prefix.length).trim().split(/ +/);
-  const command = args.shift().toLowerCase();
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-  if (command === 'ping') {
-    message.channel.send('Pong.');
-  } else if (command === 'hello') {
-    message.channel.send(`Hello!, ${message.author}`);
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
   }
-});
+}
 
-// When the client is ready, run this code (only once)
-client.once(Events.ClientReady, readyClient => {
-  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 // Log in to Discord with your client's token
 client.login(token);
