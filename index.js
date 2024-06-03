30c920f1d58eb18e2c19b0cd58b6b57c55a88ff9
anuj-weebjs <weeb.js@yahoo.com>
2024-06-03 11:28:36 +0530

    optimized code added .envexample

--- index.js
+++ index.js
@@ -1,58 +1,82 @@
-// File system module - Node's native - used to read commands dir
-const fs = require('node:fs');
-// Path utility to construct paths to access files and dirs
-const path = require('node:path');
 require('dotenv').config();
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
 
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-
-
-// Collection class extends JavaScripts native Map class, and has more extensive and useful functionality.
-// Used to store and effeciently retrieve commands for execution
-
-// Create a new client instance
-const token = process.env.TOKEN;
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    // GatewayIntentBits.GuildMessages,
-    // GatewayIntentBits.MessageContent,
-  ]
-});
+const client = new Client({ intents: GatewayIntentBits.Guilds });
 client.commands = new Collection();
+client.commandArray = [];
+client.buttons = new Collection();
 
-// Importing commands from commands folder using fs and path
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
+const directoryPath = path.join(__dirname, 'functions');
+const functionFolders = fs.readdirSync(directoryPath);
 
-for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    // Set a new item in the Collection with the key as the command name and the value as the exported module
-    if ('data' in command && 'execute' in command) {
-      client.commands.set(command.data.name, command);
-    } else {
-      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-    }
-  }
+for (const folder of functionFolders) {
+    const functionFiles = fs.readdirSync(path.join(directoryPath, folder)).filter(file => file.endsWith('.js'));
+    for (const file of functionFiles) require(path.join(directoryPath, folder, file))(client);
 }
 
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+// client.handleComponents();
+client.handleEvents();
+client.login(process.env.TOKEN);
 
-for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-}
 
-// Log in to Discord with your client's token
-client.login(token);
+
+// // File system module - Node's native - used to read commands dir
+// const fs = require('node:fs');
+// // Path utility to construct paths to access files and dirs
+// const path = require('node:path');
+// require('dotenv').config();
+
+// const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+
+
+// // Collection class extends JavaScripts native Map class, and has more extensive and useful functionality.
+// // Used to store and effeciently retrieve commands for execution
+
+// // Create a new client instance
+// const token = process.env.TOKEN;
+// const client = new Client({
+//   intents: [
+//     GatewayIntentBits.Guilds,
+//     // GatewayIntentBits.GuildMessages,
+//     // GatewayIntentBits.MessageContent,
+//   ]
+// });
+// client.commands = new Collection();
+
+// // Importing commands from commands folder using fs and path
+// const foldersPath = path.join(__dirname, 'commands');
+// const commandFolders = fs.readdirSync(foldersPath);
+
+// for (const folder of commandFolders) {
+//   const commandsPath = path.join(foldersPath, folder);
+//   const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+//   for (const file of commandFiles) {
+//     const filePath = path.join(commandsPath, file);
+//     const command = require(filePath);
+//     // Set a new item in the Collection with the key as the command name and the value as the exported module
+//     if ('data' in command && 'execute' in command) {
+//       client.commands.set(command.data.name, command);
+//     } else {
+//       console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+//     }
+//   }
+// }
+
+// const eventsPath = path.join(__dirname, 'events');
+// const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+// for (const file of eventFiles) {
+// 	const filePath = path.join(eventsPath, file);
+// 	const event = require(filePath);
+// 	if (event.once) {
+// 		client.once(event.name, (...args) => event.execute(...args));
+// 	} else {
+// 		client.on(event.name, (...args) => event.execute(...args));
+// 	}
+// }
+
+// // Log in to Discord with your client's token
+// client.login(token);
